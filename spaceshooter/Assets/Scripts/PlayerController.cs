dea6a99d869c6c94a8e29924c93e1219698e4922
wunderbarr <wunderbardan@gmail.com>
2017-09-15 17:01:08 +0200

    　velocity

---
+++ spaceshooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody rb;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.velocity = movement;
+
+
+    }
+}
