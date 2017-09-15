9be9a0aca7c98f835625fc450e4dca3cdbf39b8b
wunderbarr <wunderbardan@gmail.com>
2017-09-15 17:37:15 +0200

    　boundary and tilt

--- spaceshooter/Assets/Scripts/PlayerController.cs
+++ spaceshooter/Assets/Scripts/PlayerController.cs
@@ -2,8 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
     private Rigidbody rb;
+    public float speed;
+    public Boundary boundary;
+    public float tilt;
 	
 	// Update is called once per frame
 	void Update ()
@@ -12,8 +22,16 @@ public class PlayerController : MonoBehaviour {
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        rb.velocity = movement;
+        rb =GetComponent<Rigidbody>();
+        rb.velocity = movement*speed;
 
+        rb.position = new Vector3
+        (
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax), 
+            0.0f, 
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+        );
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x*-tilt);
 
     }
 }
