60050a35b80ae53885279cc007a2ffd26da8cf00
wunderbarr <wunderbardan@gmail.com>
2017-09-16 17:05:47 +0200

    end and restart

--- spaceshooter/Assets/Scripts/GameController.cs
+++ spaceshooter/Assets/Scripts/GameController.cs
@@ -12,13 +12,33 @@ public class GameController : MonoBehaviour
     public float waveWait;
 
     public GUIText scoreText;
+    public GUIText restartText;
+    public GUIText gameOverText;
+
+    private bool gameOver;
+    private bool restart;
     private int score;
 
     void Start()
     {
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text ="";
         score = 0;
         UpdateScore();
         StartCoroutine (SpawnWaves());
+    }
+
+    void Update ()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
     }
 
     IEnumerator SpawnWaves() //coroutine
@@ -34,6 +54,13 @@ public class GameController : MonoBehaviour
                 yield return new WaitForSeconds(spawnWait);
             }
             yield return new WaitForSeconds(waveWait);
+
+            if(gameOver)
+            {
+                restartText.text = "Press 'R' to restart...";
+                restart = true;
+                break;
+            }
         }
     }
 
@@ -45,7 +72,13 @@ public class GameController : MonoBehaviour
 
     void UpdateScore()
     {
-        scoreText.text = "Scene: " + score;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver ()
+    {
+        gameOverText.text = "Game Over!";
+        gameOver = true;
     }
     
 }
