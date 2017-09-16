a2739b6f0ce6e5d02dc0c8b93bd085b9da8541c0
wunderbarr <wunderbardan@gmail.com>
2017-09-16 14:15:29 +0200

    gamecontroller

---
+++ spaceshooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject hazard;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+
+    void Start()
+    {
+        StartCoroutine (SpawnWaves());
+    }
+
+    IEnumerator SpawnWaves() //coroutine
+    {
+        yield return new WaitForSeconds(startWait);
+        for (int i=0; i < hazardCount; i++)
+        {
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+            Quaternion spawnRotation = Quaternion.identity;
+            Instantiate(hazard, spawnPosition, spawnRotation);
+            yield return new WaitForSeconds(spawnWait);
+        }
+       
+    }
+    
+}
